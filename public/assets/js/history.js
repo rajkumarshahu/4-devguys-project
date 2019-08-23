01e04a7b9031dc9f71241bd2af4b57f51253f78f
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-23 09:46:48 -0400

    cosmetic changes

--- public/assets/js/history.js
+++ public/assets/js/history.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e) {
-     
+
     function DeleteFunction(x){
         console.log(x);
     }
@@ -9,35 +9,32 @@ $(document).ready(function(e) {
             console.log(emo)
             console.log(emo.id);
             var emoName = `${emo.name}`;
-            
+
             // capitalize every letter of the emoji's name
             function EmojiName(str) {
                 emoName = str.toLowerCase().split(' ');
                 for (var x in emoName) {
-                    emoName[x] = emoName[x].charAt(0).toUpperCase() + emoName[x].substring(1);     
+                    emoName[x] = emoName[x].charAt(0).toUpperCase() + emoName[x].substring(1);
                 }
                 // Directly return the joined string
-                return emoName.join(' '); 
+                return emoName.join(' ');
              }
-             
+
             //  var emoId =  "";
              var emoId= $('tr').attr("data-id")
-    
+
             $(".report").prepend(
                 `<tr data-id='${emo.id}'>
-                    <th>${EmojiName(emoName.replace(/_/g, " "))}</th>
-                    <th>${emo.emoji}</th>
-                    <th>${emo.user_emojis.user_comment}</th>
-                    <th>
-                        <button type="button" class="btn btn-primary edit-btn" data-id='${emo.id}' id="edit-btn"><i class="far fa-edit"></i></button>
-                        <button type="button" class="btn btn-primary del-btn" data-id='${emo.id}' id="del-btn"><i class="far fa-trash-alt"></i></button
-                    </th>
+                    <td>${EmojiName(emoName.replace(/_/g, " "))}</td>
+                    <td>${emo.emoji}</td>
+                    <td>${emo.user_emojis.user_comment}</td>
+
                 </tr>`
             );//end for report's append
 
-           
+
         });//end of $.get
-             
+
         // var id;
      //function to delete the row
      $(".del-btn").click( (e) => {
@@ -45,7 +42,7 @@ $(document).ready(function(e) {
         if( e.target.parentNode.dataset.id ){
             var id = $(this).data('id');
             console.log( `- delete clicked id (${id})`,e.target.parentNode.dataset.id );
-          
+
         } else {
             console.log( `- delete clicked id (not on icon) (${id})`,e.target.dataset.id );
         }
@@ -56,15 +53,15 @@ $(document).ready(function(e) {
 
 
 
-                   
-           
+
+
     });
 
 
 
     // $(".delete-cat").on("click", function(event) {
     //     var id = $(this).data("id");
-    
+
     //     // Send the DELETE request.
     //     $.ajax("/api/cats/" + id, {
     //       type: "DELETE"
@@ -76,5 +73,5 @@ $(document).ready(function(e) {
     //       }
     //     );
     //   });
-    
+
 });
